a9d26938a37cb801a07cbfd69cc1c8d9da6eae5a
Andreas Deleuran <Andreasdelu@gmail.com>
2024-06-07 13:15:35 +0200

    Added automatic generation of hooks list in readme

---
+++ utils/updateReadmeHooks.js
@@ -0,0 +1,25 @@
+import fs from "fs/promises";
+import path from "path";
+
+const __dirname = path.resolve();
+
+async function updateReadmeHooks() {
+	const readme = await fs.readFile(path.join(__dirname, "README.md"), "utf-8");
+	const hooks = await fs
+		.readFile(path.join(__dirname, "dist", "hooks.json"), "utf-8")
+		.then((data) => JSON.parse(data))
+		.then((hooks) => {
+			return Object.entries(hooks)
+				.map(([name, { name: hookName, ts, js }]) => {
+					return `- ${hookName}`;
+				})
+				.join("\n");
+		});
+	const updatedReadme = readme.replace(
+		/<!-- hooks starts -->[\s\S]*<!-- hooks ends -->/,
+		`<!-- hooks starts -->\n${hooks}\n<!-- hooks ends -->`
+	);
+	fs.writeFile(path.join(__dirname, "README.md"), updatedReadme);
+}
+
+updateReadmeHooks();
